12948d0fb79d8c0a24c1d329cc321788dd9beef8
Renpeng Zhang <zrp1234@outlook.com>
2019-01-13 21:16:33 -0800

    add comment

--- equationSolver/equationSolver.go
+++ equationSolver/equationSolver.go
@@ -1,3 +1,5 @@
+// Package equationSolver implements util methods for solving 2 linear equations with 2 variables
+
 package equationSolver
 
 import (
@@ -29,9 +31,11 @@ type Equation struct {
 
 // Init equation obj by parsing the input equation string
 func (equaiton *Equation) Init(eString string) error {
+	// Remove extra spaces
 	space := regexp.MustCompile(`\s+`)
 	eString = space.ReplaceAllString(eString, "")
 
+	// Detect empty string
 	if len(eString) == 0 {
 		return errors.New("Empty String")
 	}
@@ -39,9 +43,13 @@ func (equaiton *Equation) Init(eString string) error {
 	valueBeenSet := false
 	beg := 0
 	sign := 1.0
+
+	// Walk through the string to build Equation obj
 	for i := 0; i < len(eString); i++ {
 		curChar := (rune)(eString[i])
+
 		if unicode.IsLetter(curChar) {
+			// Found variable
 			coefficient := sign
 			if beg != i {
 				f, err := strconv.ParseFloat(eString[beg:i], 64)
@@ -52,6 +60,7 @@ func (equaiton *Equation) Init(eString string) error {
 			}
 			equaiton.elements = append(equaiton.elements, VariableAndCoef{curChar, coefficient})
 		} else if curChar == '=' {
+			// Set right value of the equation
 			f, err := strconv.ParseFloat(eString[i+1:len(eString)], 64)
 			if err != nil {
 				return errors.New("Invalid String")
@@ -68,19 +77,23 @@ func (equaiton *Equation) Init(eString string) error {
 		}
 	}
 
+	// Invalid string if there is no or more than 3 varaiables, or no right value
 	if (len(equaiton.elements) != 2 && len(equaiton.elements) != 1) || !valueBeenSet {
 		return errors.New("Invalid String")
 	}
 
+	// Invalid string if 2 varibales share the same name
 	if len(equaiton.elements) == 2 && (equaiton.elements[0].Variable == equaiton.elements[1].Variable) {
 		return errors.New("Invalid String")
 	}
+
 	return nil
 }
 
 // Solve the equation
 func Solve(equaiton1 *Equation, equaiton2 *Equation) ([]VariableAndValue, error) {
 	if len(equaiton1.elements) == 1 && len(equaiton2.elements) == 1 {
+		// Case that both equations have only 1 variable and needed to be completed
 		if equaiton1.elements[0].Variable == equaiton2.elements[0].Variable {
 			return nil, errors.New("Not Match Equation")
 		} else {
@@ -88,12 +101,14 @@ func Solve(equaiton1 *Equation, equaiton2 *Equation) ([]VariableAndValue, error)
 			equaiton2.elements = append(equaiton2.elements, VariableAndCoef{equaiton1.elements[0].Variable, 0})
 		}
 	} else if len(equaiton1.elements) == 1 {
+		// Case that 1 equation has only 1 variable and needed to be completed
 		if equaiton1.elements[0].Variable == equaiton2.elements[0].Variable {
 			equaiton1.elements = append(equaiton1.elements, VariableAndCoef{equaiton2.elements[1].Variable, 0})
 		} else {
 			equaiton1.elements = append(equaiton1.elements, VariableAndCoef{equaiton2.elements[0].Variable, 0})
 		}
 	} else if len(equaiton2.elements) == 1 {
+		// Case that 1 equation has only 1 variable and needed to be completed
 		if equaiton2.elements[0].Variable == equaiton1.elements[0].Variable {
 			equaiton2.elements = append(equaiton2.elements, VariableAndCoef{equaiton1.elements[1].Variable, 0})
 		} else {
@@ -101,6 +116,7 @@ func Solve(equaiton1 *Equation, equaiton2 *Equation) ([]VariableAndValue, error)
 		}
 	}
 
+	// Sort the variables for both equations
 	sort.Slice(equaiton1.elements, func(i, j int) bool {
 		return equaiton1.elements[i].Variable < equaiton1.elements[j].Variable
 	})
@@ -116,10 +132,12 @@ func Solve(equaiton1 *Equation, equaiton2 *Equation) ([]VariableAndValue, error)
 	value1 := equaiton1.value
 	value2 := equaiton2.value
 
+	// Case that the variables' names are not matched
 	if (VC11.Variable != VC21.Variable) || (VC12.Variable != VC22.Variable) {
 		return nil, errors.New("Not Match Equation")
 	}
 
+	// Case that 2 equations are relative
 	if math.Abs(VC11.Coef*VC22.Coef-VC12.Coef*VC21.Coef) < 0.0000001 {
 		if math.Abs(value1*VC21.Coef-value2*VC11.Coef) < 0.0000001 {
 			return nil, errors.New("Infinite Number of Solutions")
@@ -128,6 +146,7 @@ func Solve(equaiton1 *Equation, equaiton2 *Equation) ([]VariableAndValue, error)
 		}
 	}
 
+	// Solve the equations
 	valueForVariable1 := (value1*VC22.Coef - value2*VC12.Coef) / (VC11.Coef*VC22.Coef - VC12.Coef*VC21.Coef)
 	valueForVariable2 := (value2*VC11.Coef - value1*VC21.Coef) / (VC11.Coef*VC22.Coef - VC12.Coef*VC21.Coef)
 
--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Package main implements RESTful service for solving 2 linear equations with 2 variables
+
 package main
 
 import (
@@ -14,14 +16,16 @@ import (
 
 // GetCalculation handles the calculation request
 func GetCalculation(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get calculation request")
 	equation1 := new(equationSolver.Equation)
 	equation2 := new(equationSolver.Equation)
 	log.Printf("Equation1: %s   Equation2: %s\n", r.FormValue("equation1"), r.FormValue("equation2"))
 
+	// Init both equations
 	initErr1 := equation1.Init(r.FormValue("equation1"))
 	initErr2 := equation2.Init(r.FormValue("equation2"))
+
 	if initErr1 != nil || initErr2 != nil {
+		// Detect invalid input
 		w.WriteHeader(http.StatusBadRequest)
 		if initErr1 != nil {
 			fmt.Fprint(w, initErr1)
@@ -32,6 +36,7 @@ func GetCalculation(w http.ResponseWriter, r *http.Request) {
 		}
 
 	} else {
+		// Solve the problem
 		solution, calErr := equationSolver.Solve(equation1, equation2)
 		if calErr != nil {
 			w.WriteHeader(http.StatusBadRequest)
@@ -44,24 +49,24 @@ func GetCalculation(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// hello handles main page request
-func hello(w http.ResponseWriter, r *http.Request) {
+// Hello handles main page request
+func Hello(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
 func main() {
+	// Open log file for logging
 	logFileName := "app.log"
 	logFile, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-
 	if err != nil {
 		log.Fatalf("Cannot opening file: %s", logFileName)
 	}
 	defer logFile.Close()
 	log.SetOutput(logFile)
 
+	// RESTful service
 	router := mux.NewRouter()
-	router.HandleFunc("/", hello)
+	router.HandleFunc("/", Hello)
 	router.HandleFunc("/calculateEquation", GetCalculation).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
-	// comment
 }
